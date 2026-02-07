fa0592efda27ee007a51366b943d7041146b3fd1
Kenneth-Antonil <payment.kraph@gmail.com>
2026-02-07 12:49:43 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,48 @@
-const CACHE_NAME = 'radio-bingo-final-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+const firebaseConfig = {
+    apiKey: "AIzaSyDIAQXJ15atKJxu7PtcFL1W9JnO1N14pVs",
+    authDomain: "radiobingo-9ac29.firebaseapp.com",
+    databaseURL: "https://radiobingo-9ac29-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "radiobingo-9ac29",
+    storageBucket: "radiobingo-9ac29.firebasestorage.app",
+    messagingSenderId: "965903993397",
+    appId: "1:965903993397:web:f6646fa05225f147eebf7c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Ito ang tatanggap ng message kahit NAKA-CLOSE ang app
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/3665/3665939.png',
+    vibrate: [200, 100, 200],
+    data: { url: './index.html' }
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+// Pag kinlick ang notification, bubuksan ang app
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then( windowClients => {
+        for (var i = 0; i < windowClients.length; i++) {
+            var client = windowClients[i];
+            if (client.url.indexOf('index.html') !== -1 && 'focus' in client) {
+                return client.focus();
+            }
+        }
+        if (clients.openWindow) {
+            return clients.openWindow('./index.html');
+        }
     })
-  );
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
